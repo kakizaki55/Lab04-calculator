a3f9ed0361a2c76eefaa4c7a8a463779fd1da497
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-08-26 11:36:27 -0700

    test done, vvariables done

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // import functions
 
+const addNum1 = document.getElementById('addnNum1');
+const addNum2 = document.getElementById('addnNum2');
+
+const addButton = document.getElementById('addButton');
+const answer = document.getElementById('answer');
 // reference needed DOM elements
 
 
 // set event listeners 
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+// get user input(s)
+// do any needed work with the value(s)
+// update DOM to reflect new value(s)
--- test/tests.js
+++ test/tests.js
@@ -3,15 +3,22 @@
 
 const test = QUnit.test;
 
+function add(nm1, nm2) {
+    return nm1 + nm2;
+    
+}
+
 // name your test by what it is testing
-test('time to test a function', (expect) => {
+test('add two num', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 10;
+    const y = 5;
+    const expected = 15;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = add(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
